4786ac391673e99b022ad57daf96ce86f5e20b65
Alex Theedom <alex.theedom@gmail.com>
2019-02-17 19:38:27 +0000

    Use @WebServlet instead of DD

--- Chapter-3-Servlet-4-0/src/main/java/com/readlearncode/serlvet/request/RequestPath.java
+++ Chapter-3-Servlet-4-0/src/main/java/com/readlearncode/serlvet/request/RequestPath.java
@@ -1,5 +1,6 @@
 package com.readlearncode.serlvet.request;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import java.io.PrintWriter;
  * @author Alex Theedom www.readlearncode.com
  * @version 1.0
  */
+@WebServlet("/request-path/*")
 public class RequestPath extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
