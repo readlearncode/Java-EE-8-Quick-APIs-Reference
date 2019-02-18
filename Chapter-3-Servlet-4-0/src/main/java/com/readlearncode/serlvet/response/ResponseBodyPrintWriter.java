9a2595fd31f9d3fc781bfe760581a235351fa16a
Alex Theedom <alex.theedom@gmail.com>
2019-02-18 21:58:01 +0000

    Add example using PrintWriter

---
+++ Chapter-3-Servlet-4-0/src/main/java/com/readlearncode/serlvet/response/ResponseBodyPrintWriter.java
@@ -0,0 +1,30 @@
+package com.readlearncode.serlvet.response;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@WebServlet("/print-writer")
+public class ResponseBodyPrintWriter extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        PrintWriter writer = response.getWriter();
+
+        writer.write("<b>The Response Body</b><br>");
+
+        writer.append("----------------------<br>");
+
+        writer.format("Format a String: %s" , "This is a String value");
+    }
+
+}
